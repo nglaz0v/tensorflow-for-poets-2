83fbad2762b9e031dd2cf5a985c28a94999857b2
Nikolay Glazov <glazov_n_o@mail.ru>
2023-05-10 17:58:54 +0300

    Tensorflow 1.x compatibility

--- scripts/label_image.py
+++ scripts/label_image.py
@@ -23,15 +23,16 @@ import time
 
 import numpy as np
 import tensorflow as tf
+tf.compat.v1.disable_eager_execution()
 
 def load_graph(model_file):
-  graph = tf.Graph()
-  graph_def = tf.GraphDef()
+  graph = tf.compat.v1.Graph()
+  graph_def = tf.compat.v1.GraphDef()
 
   with open(model_file, "rb") as f:
     graph_def.ParseFromString(f.read())
   with graph.as_default():
-    tf.import_graph_def(graph_def)
+    tf.compat.v1.import_graph_def(graph_def)
 
   return graph
 
@@ -39,30 +40,30 @@ def read_tensor_from_image_file(file_name, input_height=299, input_width=299,
 				input_mean=0, input_std=255):
   input_name = "file_reader"
   output_name = "normalized"
-  file_reader = tf.read_file(file_name, input_name)
+  file_reader = tf.compat.v1.read_file(file_name, input_name)
   if file_name.endswith(".png"):
-    image_reader = tf.image.decode_png(file_reader, channels = 3,
+    image_reader = tf.compat.v1.image.decode_png(file_reader, channels = 3,
                                        name='png_reader')
   elif file_name.endswith(".gif"):
-    image_reader = tf.squeeze(tf.image.decode_gif(file_reader,
+    image_reader = tf.compat.v1.squeeze(tf.compat.v1.image.decode_gif(file_reader,
                                                   name='gif_reader'))
   elif file_name.endswith(".bmp"):
-    image_reader = tf.image.decode_bmp(file_reader, name='bmp_reader')
+    image_reader = tf.compat.v1.image.decode_bmp(file_reader, name='bmp_reader')
   else:
-    image_reader = tf.image.decode_jpeg(file_reader, channels = 3,
+    image_reader = tf.compat.v1.image.decode_jpeg(file_reader, channels = 3,
                                         name='jpeg_reader')
-  float_caster = tf.cast(image_reader, tf.float32)
-  dims_expander = tf.expand_dims(float_caster, 0);
-  resized = tf.image.resize_bilinear(dims_expander, [input_height, input_width])
-  normalized = tf.divide(tf.subtract(resized, [input_mean]), [input_std])
-  sess = tf.Session()
+  float_caster = tf.compat.v1.cast(image_reader, tf.compat.v1.float32)
+  dims_expander = tf.compat.v1.expand_dims(float_caster, 0);
+  resized = tf.compat.v1.image.resize_bilinear(dims_expander, [input_height, input_width])
+  normalized = tf.compat.v1.divide(tf.compat.v1.subtract(resized, [input_mean]), [input_std])
+  sess = tf.compat.v1.Session()
   result = sess.run(normalized)
 
   return result
 
 def load_labels(label_file):
   label = []
-  proto_as_ascii_lines = tf.gfile.GFile(label_file).readlines()
+  proto_as_ascii_lines = tf.compat.v1.gfile.GFile(label_file).readlines()
   for l in proto_as_ascii_lines:
     label.append(l.rstrip())
   return label
@@ -121,7 +122,7 @@ if __name__ == "__main__":
   input_operation = graph.get_operation_by_name(input_name);
   output_operation = graph.get_operation_by_name(output_name);
 
-  with tf.Session(graph=graph) as sess:
+  with tf.compat.v1.Session(graph=graph) as sess:
     start = time.time()
     results = sess.run(output_operation.outputs[0],
                       {input_operation.outputs[0]: t})
